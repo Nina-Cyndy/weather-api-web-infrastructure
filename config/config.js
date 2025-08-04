a0e74a0863ca8c6d85efde0d8785df9fbc476dab
Bwiza Cyndy Nina <n.bwiza@alustudent.com>
2025-08-04 20:15:27 +0200

    update

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 import keyManager from './keyManager.js';
 
 export default {
-  AIR_KEY: keyManager.getKey('AIR_KEY'),
-  API_KEY: keyManager.getKey('API_KEY'),
-  API_URL: keyManager.getKey('API_URL'),
-  AIR_URL: keyManager.getKey('AIR_URL'),
+  AIR_KEY: process.env.AIR_KEY ? keyManager.decode(process.env.AIR_KEY) : keyManager.getKey('AIR_KEY'),
+  API_KEY: process.env.API_KEY ? keyManager.decode(process.env.API_KEY) : keyManager.getKey('API_KEY'),
+  API_URL: process.env.API_URL ? keyManager.decode(process.env.API_URL) : keyManager.getKey('API_URL'),
+  AIR_URL: process.env.AIR_URL ? keyManager.decode(process.env.AIR_URL) : keyManager.getKey('AIR_URL'),
 }
